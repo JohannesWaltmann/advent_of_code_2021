af9b04423896c00f2f2a548779f1c08360f63fa5
Webbased Toast <j.waltmann@gmx.de>
2021-12-08 14:43:35 +0100

    Added day 3 task 01

--- day_3/day3.py
+++ day_3/day3.py
@@ -1,34 +1,61 @@
-import numpy as np
-
-test_data = [str(x).strip() for x in open('test_data').readlines()]
-val_data = [str(x).strip() for x in open('val_data').readlines()]
+test_data = [str(x).strip() for x in open('ressources/test_data', 'r').readlines()]
+val_data = [str(x).strip() for x in open('ressources/val_data', 'r').readlines()]
 
 
-def get_gamma_rate(data):
-    _count_zeroes = 0
-    _count_ones = 0
+def get_gamma_rate(data) -> str:
+
     _output = ''
 
-    for elem in data:
-        print(elem)
-        for bit in range(len(elem)):
-            print(elem[bit])
-            if elem[bit] == '0':
+    for elem in range(len(data[0])):
+        _count_zeroes = 0
+        _count_ones = 0
+
+        for bit in range(len(data)):
+            if int(data[bit][elem]) == 0:
                 _count_zeroes += 1
-            elif elem[bit] == '1':
+            elif int(data[bit][elem]) == 1:
                 _count_ones += 1
 
         if _count_ones > _count_zeroes:
-            print(f'output: {_output}')
             _output += '1'
         else:
-            print(f'output: {_output}')
             _output += '0'
-
-        _count_ones = 0
-        _count_zeroes = 0
 
     return _output
 
 
-print(get_gamma_rate(test_data))
+def get_epsilon_rate(data) -> str:
+
+    _output = ''
+
+    for elem in range(len(data[0])):
+        _count_zeroes = 0
+        _count_ones = 0
+
+        for bit in range(len(data)):
+            if int(data[bit][elem]) == 0:
+                _count_zeroes += 1
+            elif int(data[bit][elem]) == 1:
+                _count_ones += 1
+
+        if _count_ones > _count_zeroes:
+            _output += '0'
+        else:
+            _output += '1'
+
+    return _output
+
+
+def calc_power_consumption(data) -> int:
+    gamma_rate, epsilon_rate = get_gamma_rate(data), get_epsilon_rate(data)
+
+    return int(gamma_rate, 2) * int(epsilon_rate, 2)
+
+
+""" Eval test_data. """
+print(f'Task 01 tests: {calc_power_consumption(test_data)}')
+# print(f'Task 02 tests: {}')
+
+""" Eval val_data. """
+print(f'Task 01 validation: {calc_power_consumption(val_data)}')
+# print(f'Task 02 validation: {}')
