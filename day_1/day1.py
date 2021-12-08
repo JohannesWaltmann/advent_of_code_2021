df5d71fb52221c7b2a9f9a8e355c9b2cdf737226
Webbased Toast <j.waltmann@gmx.de>
2021-12-08 12:09:32 +0100

    day_1 cleanup and overhaul

--- day_1/day1.py
+++ day_1/day1.py
@@ -1,12 +1,40 @@
 import numpy as np
 
-test_data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-val_data = np.loadtxt('in', dtype=int)
-print(val_data)
-counter = 0
 
-for i in range(3, len(val_data)):
-    if (val_data[i]) > (val_data[i-3]):
-        counter += 1
+def import_data(filename) -> []:
+    return np.loadtxt(filename, dtype=int)
 
-print(counter)
+
+def count_measurements(set_name) -> int:
+    dataset = import_data(set_name)
+    _counter = 0
+
+    for measurement in range(len(dataset)):
+        if dataset[measurement] > dataset[measurement - 1]:
+            _counter += 1
+
+    return _counter
+
+
+def count_measurements_windowed(set_name) -> int:
+    _dataset = import_data(set_name)
+    _counter = 0
+
+    for i in range(3, len(_dataset)):
+        if (_dataset[i]) > (_dataset[i - 3]):
+            _counter += 1
+
+    return _counter
+
+
+""" Test implementation """
+task_01_test = count_measurements('test_data')
+task_02_test = count_measurements_windowed('test_data')
+
+print(f'Task 01 test: {task_01_test}\nTask 02 test: {task_02_test}')
+
+""" Solution Output """
+task_01_output = count_measurements('val_data')
+task_02_output = count_measurements_windowed('val_data')
+
+print(f'Task 01 output: {task_01_output}\nTask 02 output: {task_02_output}')
