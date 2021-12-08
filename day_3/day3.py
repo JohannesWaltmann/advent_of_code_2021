1453bbb42a7fdd1275332f5e0ac2370bea018119
Webbased Toast <j.waltmann@gmx.de>
2021-12-08 16:34:49 +0100

    Added day 3 task 02

--- day_3/day3.py
+++ day_3/day3.py
@@ -3,7 +3,6 @@ val_data = [str(x).strip() for x in open('ressources/val_data', 'r').readlines()
 
 
 def get_gamma_rate(data) -> str:
-
     _output = ''
 
     for elem in range(len(data[0])):
@@ -25,7 +24,6 @@ def get_gamma_rate(data) -> str:
 
 
 def get_epsilon_rate(data) -> str:
-
     _output = ''
 
     for elem in range(len(data[0])):
@@ -52,10 +50,76 @@ def calc_power_consumption(data) -> int:
     return int(gamma_rate, 2) * int(epsilon_rate, 2)
 
 
+def get_oxygen(data, index=0):
+    index_inc = index
+    cache_save = data
+
+    while len(cache_save) >= 1:
+        for elem in range(len(cache_save[0])):
+            _count_zeroes = 0
+            _count_ones = 0
+
+            for bit in range(len(cache_save)):
+                if int(cache_save[bit][elem]) == 0:
+                    _count_zeroes += 1
+                elif int(cache_save[bit][elem]) == 1:
+                    _count_ones += 1
+
+            if _count_ones > _count_zeroes:
+                cache_save = [x for x in cache_save if x[index_inc] == '1']
+            elif _count_zeroes > _count_ones:
+                cache_save = [x for x in cache_save if x[index_inc] == '0']
+            else:
+                cache_save = [x for x in cache_save if x[index_inc] == '1']
+            index_inc += 1
+
+        if len(cache_save) == 1: break
+        get_oxygen(cache_save, index_inc)
+
+    return cache_save
+
+
+def get_co2(data, index=0):
+    index_inc = index
+    cache_save = data
+
+    while len(cache_save) > 1:
+        for elem in range(len(cache_save[0])):
+            _count_zeroes = 0
+            _count_ones = 0
+
+            for bit in range(len(cache_save)):
+                if int(cache_save[bit][elem]) == 0:
+                    _count_zeroes += 1
+                elif int(cache_save[bit][elem]) == 1:
+                    _count_ones += 1
+
+            if _count_ones > _count_zeroes:
+                cache_save = [x for x in cache_save if x[index_inc] == '0']
+            elif _count_zeroes > _count_ones:
+                cache_save = [x for x in cache_save if x[index_inc] == '1']
+            else:
+                cache_save = [x for x in cache_save if x[index_inc] == '0']
+            index_inc += 1
+
+            if len(cache_save) == 1: break
+        get_oxygen(cache_save, index_inc)
+
+    return cache_save
+
+
+def calc_life_support_rate(data) -> int:
+    oxygen = get_oxygen(data)[0]
+    co2 = get_co2(data)[0]
+
+    return int(oxygen, 2) * int(co2, 2)
+
+
 """ Eval test_data. """
-print(f'Task 01 tests: {calc_power_consumption(test_data)}')
-# print(f'Task 02 tests: {}')
+print(f'Task 01 tests power consumption: {calc_power_consumption(test_data)}')
+print(f'Task 02 tests oxygen: {get_oxygen(test_data)[0]}')
+print(f'Task 02 tests co2: {get_co2(test_data)}')
 
 """ Eval val_data. """
 print(f'Task 01 validation: {calc_power_consumption(val_data)}')
-# print(f'Task 02 validation: {}')
+print(f'Task 02 validation: {calc_life_support_rate(val_data)}')
