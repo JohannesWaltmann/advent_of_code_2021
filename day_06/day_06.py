666bbdb81cec404b2132b18cd491027a5ac4ff50
Webbased Toast <j.waltmann@gmx.de>
2021-12-09 19:13:12 +0100

    Finished day 06 task 02

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -5,9 +5,36 @@ validation_population = np.loadtxt('resources/validation_population', dtype=int,
 
 
 def t01_calc_population(base_population) -> int:
-
-    working_population = np.copy(base_population)
+    working_population = list(base_population)
     cycle = 80
 
-    for round in cycle:
+    while cycle > 0:
+        for fish in range(len(working_population)):
+            if working_population[fish] > 0:
+                working_population[fish] -= 1
+            else:
+                working_population[fish] = 6
+                working_population.append(8)
+        cycle -= 1
 
+    return len(working_population)
+
+
+def t02_calc_population(base_population) -> int:
+
+    arr = [0 for i in range(9)]
+    for i in base_population:
+        arr[i] += 1
+
+    for i in range(256):
+        births = arr.pop(0)
+        arr.append(births)
+        arr[6] += births
+    return sum(arr)
+
+
+print(f"Task 01 test solution: {t01_calc_population(test_population)}")
+print(f"Task 01 validation solution: {t01_calc_population(validation_population)}")
+
+print(f"Task 02 test solution: {t02_calc_population(test_population)}")
+print(f"Task 02 validation solution: {t02_calc_population(validation_population)}")
