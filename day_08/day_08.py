428ef5cca5720910d0df00d60b1c65821ac6d83f
Webbased Toast <j.waltmann@gmx.de>
2021-12-09 22:05:39 +0100

    Added first semi-working solution for task 01

--- day_08/day_08.py
+++ day_08/day_08.py
@@ -6,10 +6,16 @@ test_input = [[num.strip().split(' ') for num in line.split('|')]
 val_input = [[num.strip().split(' ') for num in line.split('|')]
              for line in open('resources/val_input', 'r').readlines()]
 
+print(test_input)
 
-def find_digit_in_segments(segments : list) -> int:
 
-    return 0
+def find_digit_in_segments(display: list) -> int:
+    num_digits = 0
+    for digit in display:
+        for segment in digit[1]:
+            if len(segment) == 2 or len(segment) == 3 or len(segment) == 4 or len(segment) == 7:
+                num_digits += 1
+    return num_digits
 
 
 print(f'Solution test task 01: {find_digit_in_segments(test_input)}')
